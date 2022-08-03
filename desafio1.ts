f05ca4e3e3dd5c70f7b897e9d21ba708da07bbe5
Rauã <rauajosebr@gmail.com>
2022-08-03 13:42:31 -0300

    tentativas de resolucoes

---
+++ desafio1.ts
@@ -0,0 +1,12 @@
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+// Possivel solução
+
+let funcionario = {
+    codigo: 18,
+    nome: 'Raua'
+    
+};
+let funcionario2: {codigo: number, nome: string} = {
+    codigo: 18,
+    nome: 'Raua'
+}
